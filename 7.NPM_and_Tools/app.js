c75e604c91d70685ef6ffae92967cb7fe582df66
Shreya <shreya16180@gmail.com>
2025-03-17 17:36:56 +0530

    nodemon

---
+++ 7.NPM_and_Tools/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+	console.log(req);
+});
+
+const PORT = 3022;
+server.listen(PORT, () => {
+	console.log(`Server is running on : http://localhost:${PORT}`);
+});
